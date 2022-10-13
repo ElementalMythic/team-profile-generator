6a090e57d0848dbb7ce5899fa2dfdb5e8a01fd4d
Ryan Gustafson <ryangustafson@ryans-mac-mini.lan>
2022-10-13 11:18:50 -0500

    added sude for index and updated package.json

---
+++ index.js
@@ -0,0 +1,28 @@
+// Here is where I will need to add my imports
+
+
+
+
+
+// Need to make a list for the employees
+
+
+
+
+
+// Need to promt the user to add the employee 
+// Need to set the values for employees
+//Need to return this
+
+// Need to make sure that all catagories requires are included for said emplyee
+
+// need to make function 
+
+// need to generate the html
+
+// loop to make sure they can add as many employees as needed
+
+// need to have a stop button
+
+// init();
+
